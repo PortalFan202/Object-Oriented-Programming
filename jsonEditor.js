81a5a8bed45bd59219454fe2f1e840e7ea5617ef
PortalFan202 <61405269+PortalFan202@users.noreply.github.com>
2022-02-28 21:01:35 +0200

    Delete jsonEditor.js

--- jsonEditor.js
+++
@@ -1,9 +0,0 @@
-const jsonFile = require("./products.json")
-
-for (let index = 0; index < jsonFile.length; index++) {
-    const element = jsonFile[index];
-    element.image = "./images/";
-}
-
-console.log(JSON.stringify(jsonFile))
-
